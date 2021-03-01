b75823fe13ad3a67596375b2ae180c528761077c
BorjasRaul <79670821+BorjasRaul@users.noreply.github.com>
2021-03-01 14:34:36 -0600

    imagenes

--- app/src/main/java/com/example/eventos/MainActivity.kt
+++ app/src/main/java/com/example/eventos/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
 
     //hola
+
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>? = null
     lateinit var recyclerView: RecyclerView
